4db988c480f3abfcfd404cbd6d194211e7ee0463
gowri06 <gowripadmanathan@gmail.com>
2017-06-05 20:59:56 +0530

    Create Po.java

---
+++ Po.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class Po
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a,b;
+if(a>o)
+{
+System.out.println("positive value"+=);
+}
+else
+{
+System.out.println("negative value"+=);
+}
+}
+}
