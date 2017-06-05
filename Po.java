2f82b2fa70e0a6da44bc5cf458562f55b500420c
gowri06 <gowripadmanathan@gmail.com>
2017-06-05 21:28:09 +0530

    Update Po.java

--- Po.java
+++ Po.java
@@ -1,17 +1,41 @@
-import java.io.*;
-class Po
-{
-public static void main(String args[])
-{
-Scanner s=new Scanner(System.in);
-int a,b;
-if(a>o)
-{
-System.out.println("positive value"+=);
-}
-else
-{
-System.out.println("negative value"+=);
-}
-}
-}
+    import java.util.Scanner;
+  public class Po
+  {
+      public static void main(String[] args) 
+      {
+
+            int n;
+
+            Scanner s = new Scanner(System.in);
+
+            System.out.print("Enter the number:");
+
+            n = s.nextInt();
+
+            if(n > 0)
+
+            {
+
+                System.out.println("The given number "+n+" is Positive");
+
+            }
+
+            else if(n < 0)
+
+            {
+
+                System.out.println("The given number "+n+" is Negative");
+
+            }
+
+            else
+
+            {
+
+                System.out.println("The given number "+n+" is neither Positive nor Negative ");
+
+            }
+
+        }
+
+    }
